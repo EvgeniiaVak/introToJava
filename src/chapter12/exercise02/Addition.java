fa4db66d717d1fedab1ce46f080105188cf1deae
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2017-09-12 13:49:36 +0700

    Fix exercise 12.02
    Remove scanner from loop

--- src/chapter12/exercise02/Addition.java
+++ src/chapter12/exercise02/Addition.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 
 public class Addition {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("enter two integers");
 
             try {
+
                 int a = scanner.nextInt();
                 int b = scanner.nextInt();
                 System.out.println("their sum is " + (a + b));
                 break;
+
             } catch (InputMismatchException e) {
                 System.out.println("wrong input");
+                //discard input
+                scanner.nextLine();
             }
         }
     }
